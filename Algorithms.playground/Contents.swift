7d4a164d8d77f006a237d2bab91003f095b32f8d
efrosinina <e.efrosininaa@gmail.com>
2023-08-15 22:36:25 +0300

    *main Big O Notation

--- Algorithms.playground/Contents.swift
+++ Algorithms.playground/Contents.swift
@@ -20,5 +20,38 @@ func binarySearch(_ list: [Int], item: Int) -> Int? {
     return nil
 }
 
-let myList = [1, 3, 5, 7, 9]
+let myList = [0, 1, 3, 5, 5, 7, 9]
 binarySearch(myList, item: 5)
+
+//O(1)
+func getFirst(_ array: [Int]) -> Int {
+    return array[0]
+}
+
+getFirst(myList)
+
+//O(N)
+func getSumm(_ array: [Int]) -> Int {
+    var sum = 0
+    for (_, item) in array.enumerated() {
+        sum = sum + item
+    }
+    return sum
+}
+
+getSumm(myList)
+
+//O(N^2)
+func getFirstPair(_ array: [Int]) -> Int? {
+    for i in 0..<array.count - 1 {
+        for j in (i + 1)..<array.count {
+            if array[i] == array[j] {
+                return array[i]
+            }
+        }
+    }
+    return nil
+}
+
+getFirstPair(myList)
+
